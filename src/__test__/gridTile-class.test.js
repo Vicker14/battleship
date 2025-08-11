14876f2578e4a6a3ad7ce6ab0f91ea5ebaec03f7
Vicker <vcalero2001@gmail.com>
2025-08-11 05:32:28 +0200

    Created gameboard class file and tests

---
+++ src/__test__/gridTile-class.test.js
@@ -0,0 +1,40 @@
+const Tile = require("../scripts/gridTile-class");
+const Ship = require("../scripts/ship-class");
+
+const gridTile = new Tile();
+const boat = new Ship(2,0);
+
+test('Check tile creation', () => {
+
+    expect(gridTile.status).toBe("water")
+})
+
+test('Check if miss', () => {
+    gridTile.setTile("miss")
+
+    expect(gridTile.status).toBe("miss")
+})
+
+test('Check if hit', () => {
+    gridTile.setTile(Tile.hit)
+
+    expect(gridTile.status).toBe("hit")
+})
+
+test('Check if water', () => {
+    gridTile.setTile("water")
+
+    expect(gridTile.status).toBe("water")
+})
+
+test('Check if boat', () => {
+    gridTile.setTile(boat)
+
+    expect(gridTile.status).toBe(boat)
+})
+
+test('Check if water', () => {
+    gridTile.setTile("water")
+
+    expect(gridTile.isWater()).toBeTruthy()
+})
